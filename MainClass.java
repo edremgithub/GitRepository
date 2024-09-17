09ffcd0bfc33c23db196beb0cc563dc04f8c2825
Edrem <irlandaedrem@gmail.com>
2024-09-18 00:06:49 +0800

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MainClass{
+    public static void main(String[] args) {
+        System.out.println("Putangina ka!");
+
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Please enter your name: ");
+            String name = scan.nextLine();
+            System.out.println("Hello " + name);
+        }
+    }
+}
