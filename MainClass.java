8ecc0709cc05e6844a69361632b5333587479f58
Edrem <irlandaedrem@gmail.com>
2024-09-18 10:09:00 +0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class MainClass{
     public static void main(String[] args) {
-        System.out.println("Putangina ka!");
+        System.out.println("Welcome to Java!");
 
         try (Scanner scan = new Scanner(System.in)) {
             System.out.print("Please enter your name: ");
             String name = scan.nextLine();
-            System.out.println("Hello " + name);
+            System.out.println("Hello " + name + " Welcome to Java!");
         }
     }
 }
