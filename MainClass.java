d6b167199092c82f294e93c0b907a4ff979e9c2a
Edrem <irlandaedrem@gmail.com>
2024-09-20 23:21:32 +0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,13 +1,22 @@
+// Use this line to import Scanner and use the Scanner element so you can type in the terminal
 import java.util.Scanner;
 
-public class MainClass{
-    public static void main(String[] args) {
+// public class (Name of your class) is a standard to start a project. THIS IS A MUST!!
+public class MainClass
+{
+    public static void main(String[] args) 
+    {
+        // This first line of code will print out the words within the parentheses when you run your code or press F5
         System.out.println("Welcome to Java!");
 
-        try (Scanner scan = new Scanner(System.in)) {
+        // This code will execute the said element so you can type it in terminal
+        try (Scanner scan = new Scanner(System.in)) 
+        {
             System.out.print("Please enter your name: ");
             String name = scan.nextLine();
             System.out.println("Hello " + name + " Welcome to Java!");
         }
     }
 }
+
+// NOTICE THE OPEN AND CLOSE BRACKETS, TRY TO UNDERSTAND WHAT THEY STAND FOR AND WHAT ARE THEIR USES.
