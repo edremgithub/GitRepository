5d2b60bdef0fcd6adf39c2783b6eda682341255f
edremgithub <irlandaedrem@gmail.com>
2024-09-25 20:50:08 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,14 +7,14 @@ public class Test
     {
         System.out.println("What is your name");
 
-        try(Scanner scan = new Scanner(System.in)){
+        Scanner scan = new Scanner(System.in);
             String name = scan.nextLine();
             System.out.println("Ikaw pala si " + name + ", Kupal ka ba?");
             System.out.println("Ilang taon ka nang kupal?");
             String age = scan.nextLine();
             System.out.println( age + ", tanda mo na gago");
 
-        }
+        
 
 
        
