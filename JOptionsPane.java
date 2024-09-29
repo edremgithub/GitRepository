1f4c945439bd5841a465b29d47746b991121b696
edremgithub <irlandaedrem@gmail.com>
2024-09-29 16:03:18 +0800

    Update JOptionsPane.java

--- JOptionsPane.java
+++ JOptionsPane.java
@@ -3,8 +3,15 @@ import javax.swing.JOptionPane;
 public class JOptionsPane {
     public static void main (String[] args){
         String name = new String();
-        name = JOptionPane.showInputDialog(null, "Enter your name here: ");
+        int num = 0;
 
-        JOptionPane.showMessageDialog(null, "Your name is " + name);
+        num = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter your number"));
+
+        JOptionPane.showMessageDialog(null, num);
+
+        // This is used when you want to type letters instead of numbers
+        // name = JOptionPane.showInputDialog(null, "Enter your name here: ");
+
+        // JOptionPane.showMessageDialog(null, "Your name is " + name);
     }
 }
