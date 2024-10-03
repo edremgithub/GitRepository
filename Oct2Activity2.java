f180f4da55b309531f8029ef8851502ccd2a0b74
Edrem <irlandaedrem@gmail.com>
2024-10-03 14:56:42 +0800

    October 3 Class Activity

--- Oct2Activity2.java
+++ Oct2Activity2.java
@@ -5,22 +5,57 @@ public class Oct2Activity2 {
        
         Scanner ope = new Scanner(System.in);
         System.out.println("Enter 5 grades: ");
-        int g1 = ope.nextInt();
-        int g2 = ope.nextInt();
-        int g3 = ope.nextInt();
-        int g4 = ope.nextInt();
-        int g5 = ope.nextInt();
-        int ave = (g1+g2+g3+g4+g5)/5;
+        Double g1 = ope.nextDouble();
+        Double g2 = ope.nextDouble();
+        Double g3 = ope.nextDouble();
+        Double g4 = ope.nextDouble();
+        Double g5 = ope.nextDouble();
+        Double ave = (g1+g2+g3+g4+g5)/5;
 
         if (ave >= 90) {
-            System.out.println("Very good");
-        } else if (ave >= 80) {
-            System.out.println("Nice");
-        } else if (ave >= 75) {
-            System.out.println("Muntik na bumagsak");
-        } else {
-            System.out.println("YOU FAILED!!");
+
+            if (ave >= 95) 
+            {
+
+                if (ave >= 98) 
+                {
+                    System.out.println("A+");
+                } 
+                
+                else if (ave >= 96) 
+                {
+                    System.out.println("A");
+                }
+
+                else 
+                {
+                    System.out.println("A-");
+                }
+            }
+
         } 
         
+        else if (ave >= 80) {
+
+            if (ave >= 85) 
+            {
+
+                if (ave >= 88) 
+                {
+                    System.out.println("B+");
+                } 
+                
+                else if (ave >= 86) 
+                {
+                    System.out.println("B");
+                }
+
+                else 
+                {
+                    System.out.println("B-");
+                }
+            }
+
+        } 
     }
 }
