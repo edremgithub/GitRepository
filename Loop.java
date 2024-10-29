3e18eccd7ef2dde2d649f9ddf5be70144a21f6ac
edremgithub <irlandaedrem@gmail.com>
2024-10-29 12:29:12 +0800

    Loop

---
+++ Loop.java
@@ -0,0 +1,9 @@
+public class Loop {
+    public static void main (String[] args) {
+        int count = 1;
+        while ( count <= 5) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
