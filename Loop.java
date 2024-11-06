eae52dd6ac8d451159896d899b70213d16c49839
edremgithub <irlandaedrem@gmail.com>
2024-11-06 19:44:17 +0800

    Delete Loop.java

--- Loop.java
+++
@@ -1,9 +0,0 @@
-public class Loop {
-    public static void main (String[] args) {
-        int count = 1;
-        while ( count <= 5) {
-            System.out.println(count);
-            count++;
-        }
-    }
-}
