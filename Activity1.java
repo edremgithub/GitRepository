fa74034c9d4157d16d5f8a5d985b05fe81a94940
edremgithub <irlandaedrem@gmail.com>
2024-11-13 11:13:05 +0800

    Update Activity1.java

--- Activity1.java
+++ Activity1.java
@@ -4,8 +4,7 @@ public static void main(String[] args) {
     Scanner ojay = new Scanner(System.in);
     System.out.println("Enter Number: ");
     int n = ojay.nextInt();
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         int number = 1;
         System.out.printf("%" + (n - i) * 2 + "s", "");
         for (int j = 0; j <= i; j++) {
