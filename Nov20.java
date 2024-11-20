dc7074963469c12d088b5642e45702be44783104
Edrem <irlandaedrem@gmail.com>
2024-11-20 19:23:31 +0800

    Create Nov20.java

---
+++ Nov20.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class Nov20 {
+    public static void main (String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        // int randomNumber = (int) (Math.random() * 100) +1;
+        // int guess;
+        // int attempt = 0;
+        // System.out.println("WELCOME TO THE GUESSING GAME");
+        // do {
+        //     System.out.print("Guess a number between 1 and 100");
+            
+        //     guess = scanner.nextInt();
+        //     attempt++;
+        //         if (guess < randomNumber) {
+        //             System.out.println("Too low!");
+        //         } else if (guess > randomNumber) {
+        //             System.out.println("Too high!");
+        //         } else if (guess == randomNumber) {
+        //             System.out.println("CONGRATULATIONS!");
+        //         }
+        // } while (attempt < 5);
+
+        //ANOTHER EXAMPLE 
+
+        // char ch = 'J';
+        // if (ch >= 'A' && ch <= 'Z') {
+        //     System.out.println(ch + " is an uppercase letter");
+        // } else if ( ch >= 'a' && ch <= 'z') {
+        //     System.out.println(ch + " is a lowercase letter");
+        // } else if (ch >= '0' && ch <= '9') {
+        //     System.out.println(ch + " is a numeric character");
+        // }
+
+        //ANOTHER EXAMPLE
+
+        // String message = "Welcome to Java";
+        // System.out.println(message);
+        // System.out.println("The length of " + message + " is " + message.length());
+        // System.out.println("The first character of the message is " + message.charAt(0));
+        // System.out.println("The last character of the message is " + message.charAt(14));
+        // System.out.println("To Lower Case: " + message.toLowerCase());
+        // System.out.println("To Upper Case: " + message.toUpperCase());
+        // System.out.println("To trim: " + message.trim());
+        // System.out.println(message.substring(0, 11));
+        // System.out.println(message.substring(11));
+
+        //ANOTHER EXAMPLE
+
+        // System.out.println("Enter three words separated by spaces: ");
+        // String s1 = scanner.next();
+        // String s2 = scanner.next();
+        // String s3 = scanner.next();
+        // System.out.println("First name is: " + s1.toUpperCase());
+        // System.out.println("Middle name is: " + s2.toUpperCase());
+        // System.out.println("Last name is: " + s3.toUpperCase());
+    }
+}
